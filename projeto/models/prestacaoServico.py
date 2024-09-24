c28cc8febbdc27f905bd30953c1b26e432bccfb8
LimaNat <160279258+LimaNat@users.noreply.github.com>
2024-09-24 13:16:46 +0000

    baixar

---
+++ projeto/models/prestacaoServico.py
@@ -0,0 +1,11 @@
+import os
+os.system("cls || clear")
+
+def __init__(self, contratoInicio: str, contratoFim: str) -> None:
+    self.contratoInicio = contratoInicio
+    self.contratoFim = contratoFim
+
+def __str__(self) -> str:
+        return(
+              
+        )
